11aa77eed0ecb989ddd59fd0e00086eb05f2bf0a
Zach Rickers <mzrickers@gmail.com>
2022-04-20 10:03:11 +0200

    can make request

--- weather.py
+++ weather.py
@@ -1,2 +1,15 @@
-API_KEY = f9f2f48d25f6c8d6da3d188b16ffc993
+import requests
 
+API_KEY = "f9f2f48d25f6c8d6da3d188b16ffc993"
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("An error occured")
